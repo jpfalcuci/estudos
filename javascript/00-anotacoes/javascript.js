2aebf8c3beb484e62d7dcf3bbec7c931f67418ee
jpfalcuci <jpfalcuci@gmail.com>
2022-12-24 12:13:05 -0300

    projeto robotron (js) e update python os

--- javascript/00-anotacoes/javascript.js
+++ javascript/00-anotacoes/javascript.js
@@ -13,41 +13,93 @@
     para ativar usar "use strict" no começo do arquivo
 */
 
+// DOM => document object model
 
+document                            // é onde o js vai buscar as informações (html)
+document.getElementById('id')       // seleciona elementos pelo ID, ByClass e outros
+document.querySelector('.class')    // usa a mesma estrutura de seleção do css
+console.log('msg')                  // escreve mensagem no console
 document.write('string');
-console.log                         // exibe resultado no console
+document.classList                  // retorna um array com os nomes das classes
+document.classList[0]               // retorna uma string com a classe do index [n]
+input.value			                // para saber o valor da variável 'input'
+input.focus			                // deixa a caixa 'input' em foco
+evento.target                       // retorna o alvo da ação
+target.textContent                  // retorna o conteúdo do texto
+target.parentNode                   // retorna o pai do elemento
+button.onclick			            // executa função, chamando sem () ele só imprime a função no console
+button.oncontextmenu	            // clique direito, usar 'return false' p/ ñ exibir menu de contexto
+evento.shiftKey			            // true ou false se tecla estiver ativa, https://keycode.info
+
+
 alert('cria um popup na tela');
 prompt('popup com pergunta');
-isNaN('NaN(not a number), retorna true ou false');
-document.querySelector('input');	// pega o parâmetro 'input' do html
-input.value							// para saber o valor da variável 'input'
-input.focus							// deixa a caixa 'input' em foco
+isNaN('not a number');
+onclick="alert('msg')"          // executa função na ação do click
+alert('msg')                    // cria um popup
+setInterval(função, intervalo);	// recebe função e intervalo em milissegundos
 
-button.onclick			// executa função no clique esquerdo, chamando sem () ele só imprime a função no console
-button.oncontextmenu	// clique direito, quando não quiser exibir menu de contexto, usar 'return false' na função
-evento.shiftKey			// recebe true ou false se tecla estiver ativa, https://keycode.info
+Math.round();       // arredonda valor
+Math.floor();       // arreonda p/ baixo
+Math.random();      // gera número aleatório
+Math.PI 		    // 3,1415...
+parseInt();		    // converte string em número (se possível)
+parseFloat();	    // converte string em número mantendo as casas decimais
+
+preload();	        // carrega arquivos antes do início
+loadImage();		// exige parametro o caminho e formato da imagem
+loadSound();		// exige parametro o caminho e formato do áudio
+file.play();		// executa o arquivo de áudio
+file.loop();		// executa o arquivo em loop
+document.onclick = tocaSom();   // => executa tocaSom
+document.onclick = tocaSom;     // => guarda tocaSom dentro de onclick
+// navegadores tem política de bloquear códigos que executam mídias antes do usuário interagir com a página
+
+var ano = 2022;             //  também usa 'let'
+const constante = 'variável que não é alterada'
+var array = [0, 1, 2];      // lista
+array.length();				// quantidade de elementos do array
+array.push();				// adiciona valor p/ array
+array.forEach               // loop para arrays
+
+var tela = document.querySelector('canvas'); 	
 tela.onmousemove		// captura movimento do mouse
 tela.onmousedown		// botão esquerdo pressionado
 tela.onmouseup			// botão direito solto
 
-var ano = 2022;             // variável "ano" recebe "2022", também usa 'let'
-const mes = 12		        // declara uma constante
-var array = [0, 1, 2];      // lista
-array.length();				// retorna a quantidade de elementos do array
-array.push();				// empurra parâmetro p/ array
+var pincel = tela.getContext('2d');     // cria 'pincel'
+pincel.fillStyle = 'grey';              // cor de preenchimento do pincel
+pincel.fillRect = (xi, yi, xf, yf);     // preenche retangulo com cor, .fill preenche outras formas
+pilcel.clearRect = (xi, yi, xf, yf);    // limpa retangulo
+pincel.arc (x, y, raio, inicial, 2 * Math.PI);  // desenha círculo na tela
 
-function verbo() {
-    //cria uma função com conjunto de instruções dentro do bloco
-    return 'valor'
-}
-// funções anônimas são funções sem nomes criadas dentro de uma variável
+var x = 'parametro da função'.pageX - tela.offsetLeft;
+var y = 'parametro da função'.pageY - tela.offsetTop;   // descontar offset da tela do navegador
 
-Math.round();	// arredonda valor
-Math.floor();	// arreonda p/ baixo
-Math.random();	// gera número aleatório
-Math.PI 		// 3,1415...
-parseInt();		// converte string em número (se possível)
-parseFloat();	// converte string em número mantendo as casas decimais
+const idAudio = `#som_de_${instrumento}`;   // template string, variável dentro da string, usa cráse
+
+
+function funcao() { alert('msg') }  // função nomeada, reaproveitamento de código
+addEventListener('event', funcao)   // executa função quando ocorre evento
+
+// exemplo:
+const elemento =  document.querySelector('#elemento')
+elemento.addEventListener('click', acao)
+function acao() { console.log("mensagem") }
+
+// função anônima, executada apenas onde é declarada
+elemento.addEventListener('click', function() { console.log("mensagem") })
+// arrow function, a partir do ES6:
+elemento.addEventListener('click', () => { console.log("mensagem") })
+
+// parâmetros
+function ola(nome) { console.log("Olá " + nome) }
+elemento.addEventListener('click', (evento) => { console.log(evento) })
+
+// objeto js
+var objeto = {propriedade: "valor"}
+var pessoa = {nome: "nome", frase: "frase", enviaFrase() {return this.frase;}}
+console.log(pessoa.enviaFrase())
 
 /* 
     ;	    caractere especial que significa 'pula uma linha'
@@ -61,14 +113,14 @@ parseFloat();	// converte string em número mantendo as casas decimais
 */
 
 if(10%2==0) {
-    // se true, executa código
+    // código
 } else {
-    // código se false
+    // código
 }
 
 while(true) {
-    // enquanto true, executa código
-    break // finaliza loop (cuidado com loop infinito)
+    // código
+    break // cuidado com loop infinito
 }
 
 for (var i = 0; i <= 5; i++) {
@@ -76,34 +128,6 @@ for (var i = 0; i <= 5; i++) {
     console.log(i);
  }
  
-
-var tela = document.querySelector('canvas'); 	
-var pincel = tela.getContext('2d');			// cria 'pincel'
-pincel.fillStyle = 'grey';					// cor de preenchimento do pincel
-pincel.fillRect = (xi, yi, xf, yf);			// preenche retangulo com cor, ou .fill preenche outras formas
-pilcel.clearRect = (xi, yi, xf, yf);		// limpa retangulo
-var x = 'parametro da função'.pageX - tela.offsetLeft;
-var y = 'parametro da função'.pageY - tela.offsetTop;   // descontar offset da tela do navegador
-pincel.arc (x, y, raio, inicial, 2 * Math.PI);          // desenha círculo na tela
-
-setInterval(função, intervalo);	// recebe função e intervalo em milissegundos
-
-preload();	        // carrega arquivos antes do início
-loadImage();		// exige parametro o caminho e formato da imagem
-loadSound();		// exige parametro o caminho e formato do áudio
-file.play();		// executa o arquivo de áudio
-file.loop();		// executa o arquivo em loop
-
-document.querySelector('x');    // buscar elementos do html, padrão CSS (tag .class #id input[type=tel]) 
-
-// navegadores tem política de bloquear códigos que executa mídias antes do usuário interagir com a página
-document.onclick = tocaSom();   // => js já tenta executar imediatamente tocaSom
-document.onclick = tocaSom;     // => js guarda tocaSom dentro de onclick
-
-const idAudio = `#som_de_${instrumento}`;   // template string, variável dentro da string, usa cráse
-
-document.classList      // retorna um array com os nomes das classes
-document.classList[0]   // retorna uma string com a classe do index [n]
 
 class Classe {
     // atributos e/ou propriedades
