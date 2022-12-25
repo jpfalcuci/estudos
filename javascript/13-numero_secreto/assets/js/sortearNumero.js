411643f5b92e6c36ee1a4bf8ae7ea2d2abbe5016
jpfalcuci <jpfalcuci@gmail.com>
2022-12-25 14:59:44 -0300

    número secreto (js)

---
+++ javascript/13-numero_secreto/assets/js/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor = 1
+const maiorValor = 1000
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random()*maiorValor+1)
+};
+
+console.log("Número secreto: ",numeroSecreto)
+
+
+const elementoMenorValor = document.querySelector("#menor-valor")
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.querySelector("#maior-valor")
+elementoMaiorValor.innerHTML = maiorValor
---
+++ javascript/13-numero_secreto/assets/js/validacao.js
@@ -0,0 +1,54 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML = `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+            document.body.style.backgroundColor = "black"
+        } else {
+            elementoChute.innerHTML += '<div>Valor Inválido</div>'
+        }
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
+    }
+ 
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
